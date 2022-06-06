0393a186a5ba950f6c9697ec6815e8f3a4b1ad1c
hohice <hohice@163.com>
2022-06-06 14:17:33 +0800

    delete account

--- azure/dev/providers.tf
+++ azure/dev/providers.tf
@@ -11,9 +11,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "72876a1d-e580-4d77-b3a5-04e578a0a36a"
-  tenant_id       = "43f0b50f-1501-4ff9-9923-ed46f1aea0b8"
-  client_id       = "860dda31-f37b-4709-8a16-444c911cf3e9"
-  client_secret   = "TG~8onVCugefLvmy~BLk1iCO5kACSlJAwm"
+
 
 }
